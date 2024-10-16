0700b3433ee850ac0f7baacb3b5f99baf70723ad
Rodrigo Kikuchi <89669642+rodrigokferreira@users.noreply.github.com>
2024-10-16 12:16:39 -0300

    FindById e exceptions

--- src/main/java/com/rk/genAccess/resource/UserResource.java
+++ src/main/java/com/rk/genAccess/resource/UserResource.java
@@ -6,13 +6,14 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 import com.rk.genAccess.dto.UserDTO;
 import com.rk.genAccess.entities.User;
 import com.rk.genAccess.services.UserService;
+
 
 @RestController
 @RequestMapping(value = "/users")
@@ -26,6 +27,13 @@ public class UserResource {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream()
 				.map(x -> new UserDTO(x)).collect(Collectors.toList());
-		return ResponseEntity.ok().body(listDto);
+		return ResponseEntity.ok().body(listDto);	
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
 }	
--- src/main/java/com/rk/genAccess/services/UserService.java
+++ src/main/java/com/rk/genAccess/services/UserService.java
@@ -1,12 +1,14 @@
 package com.rk.genAccess.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rk.genAccess.entities.User;
 import com.rk.genAccess.repository.UserRepository;
+import com.rk.genAccess.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,10 @@ public class UserService {
 	public List<User> findAll() {
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+	}
+	
 }
